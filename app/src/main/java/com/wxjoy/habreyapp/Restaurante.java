3fa23a295d0a27509c1e883c384e00aafad39679
wjoelmendoza <wjoelmendoza@gmail.com>
2017-09-20 16:26:58 -0600

    Sincronizacion

---
+++ app/src/main/java/com/wxjoy/habreyapp/Restaurante.java
@@ -0,0 +1,17 @@
+package com.wxjoy.habreyapp;
+
+/**
+ * Created by wxjoy on 20/09/17.
+ */
+
+public class Restaurante {
+    private String nombre;
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public  String getNombre(){
+        return nombre;
+    }
+}
